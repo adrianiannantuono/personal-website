10f9a802d7409d90e504ac052cc1062fff55efd3
adrianiannantuono <aiannantuono@me.com>
2022-03-15 22:15:16 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,8 +13,8 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'adrianiannantuono', // Usually your GitHub org/user name.
+  projectName: 'personal-website', // Usually your repo name.
 
   presets: [
     [
